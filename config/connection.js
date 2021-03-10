16530a3e58930992e88e56ca4de1df3857859eef
Brad <bkarulas@outlook.com>
2021-03-10 13:08:19 -0500

    DB issue

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,16 @@ const mysql = require("mysql2/promise");
 require('dotenv').config();
 
 let	pool = mysql.createPool({
-		// host: process.env.DB_HOST ,
-		// database: process.env.DB_NAME ,
-		// port: process.env.DB_PORT ,
-		// user: process.env.DB_USER ,
-		// password: process.env.DB_PASSWORD
-		host: 'localhost',
-		database: 'franbrad_hkypool',
-		port: '3306',
-		user: 'franbrad_hkyadmin' ,
-		password: 'T0R0Nt)4&'
+		host: process.env.DB_HOST ,
+		database: process.env.DB_NAME ,
+		port: process.env.DB_PORT ,
+		user: process.env.DB_USER ,
+		password: process.env.DB_PASSWORD
+		// host: 'localhost',
+		// database: 'franbrad_hkypool',
+		// port: '3306',
+		// user: 'franbrad_hkyadmin' ,
+		// password: 'T0R0Nt)4&'
 
 	});
 

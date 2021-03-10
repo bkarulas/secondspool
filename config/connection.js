55949ce109abeb82bf842ad6ed34d13336e3c9f7
Brad <bkarulas@outlook.com>
2021-03-10 10:36:34 -0500

    DB

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,12 @@ const mysql = require("mysql2/promise");
 require('dotenv').config();
 
 let	pool = mysql.createPool({
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		connectionLimit: 100
+		host: process.env.DB_HOST ,
+		database: process.env.DB_NAME ,
+		port: process.env.DB_PORT ,
+		user: process.env.DB_USER ,
+		password: process.env.DB_PASSWORD ,
+		connectionLimit: DB_LIMIT
 	});
 
 module.exports = pool;

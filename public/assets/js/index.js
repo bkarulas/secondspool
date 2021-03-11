c500f3a19719438f2762a7e3b97b4e27c5156362
Brad <bkarulas@outlook.com>
2021-03-11 13:41:03 -0500

    beta version 1 updates

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,49 +1,53 @@
-let message = getUrlVar(mainUrl, 'm');
-window.onload = checkForErrors()
+//let message = getUrlVar(mainUrl, 'm');
+//window.onload = checkForErrors()
 
-document.querySelector('#login').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        enterPool();
-    }
-});
+// document.querySelector('#login').addEventListener('keypress', function (e) {
+//     if (e.key === 'Enter') {
+//         enterPool();
+//     }
+// });
 
 
 
-function checkForErrors(){
-    console.log("CHECKING");
-    messageDiv = document.getElementById('usermessage');
-    if (getUrlVar(mainUrl, 'm')){
-        if (getUrlVar(mainUrl, 'm')=='ERROR'){
-            messageDiv.innerText = "Something went wrong, please try it again"
-        }else{
-            messageDiv.innerText = "YOU ARE HERE";
-        }
-    }else{
-        messageDiv.innerText = 'Enter your email and board number'
-    }
-}
+// function checkForErrors(){
+//     console.log("CHECKING");
+//     messageDiv = document.getElementById('usermessage');
+//     if (getUrlVar(mainUrl, 'm')){
+//         if (getUrlVar(mainUrl, 'm')=='ERROR'){
+//             messageDiv.innerText = "Something went wrong, please try it again"
+//         }else{
+//             messageDiv.innerText = "YOU ARE HERE";
+//         }
+//     }else{
+//         messageDiv.innerText = 'Enter your email and board number'
+//     }
+// }
 
 async function enterPool(){
     let userEmail = document.getElementById('inputemail').value;
     let userBoard = document.getElementById('inputboard').value;
     console.log(`1. USER=${userEmail} BOARD=${userBoard} BASEURL=${baseUrl}`)
-    if (!userEmail || !userBoard){
-        if (!userEmail){
-            let emailDiv = document.getElementById('inputemail');
-            emailDiv.className = 'logintext missing';
-            emailDiv.setAttribute('placeholder','Enter a valid Email');
-        }
-        if (!userBoard){
-            let boardDiv = document.getElementById('inputboard');
-            boardDiv.className = 'logintext missing';
-            boardDiv.setAttribute('placeholder','Enter a valid Board');
-        }
-    }else{
+    // if (!userEmail || !userBoard){
+    //     if (!userEmail){
+    //         let emailDiv = document.getElementById('inputemail');
+    //         emailDiv.className = 'logintext missing';
+    //         emailDiv.setAttribute('placeholder','Enter a valid Email');
+    //     }
+    //     if (!userBoard){
+    //         let boardDiv = document.getElementById('inputboard');
+    //         boardDiv.className = 'logintext missing';
+    //         boardDiv.setAttribute('placeholder','Enter a valid Board');
+    //     }
+    // }else{
         console.log(`1. USER=${userEmail} BOARD=${userBoard} BASEURL=${baseUrl}`)
         let userId = await getUserId(userEmail, userBoard);
-        //setTimeout(letTheUserIn (userId, userBoard), 5000);
-        await letTheUserIn (userId, userBoard)
-    }
+        setTimeout(console.log("waiting"), 10000)
+        //setTimeout(await letTheUserIn (userId, userBoard), 10000);
+        let gameUrl = letTheUserIn(userId, userBoard)
+        console.log(`2. ${baseUrl+gameUrl}`);
+        window.location.href = baseUrl+gameUrl;
+
+    // }
 }
 
 async function getUserId(userEmail,userBoard){
@@ -61,15 +65,15 @@ async function getUserId(userEmail,userBoard){
     return res.json();
 }
 
-async function letTheUserIn(userId, userBoard){
+function letTheUserIn(userId, userBoard){
+    let gameUrl='';
+    console.log(`BASEURL = ${baseUrl}`)
     if (userId.length>0){
         console.log('USER')
         console.log (userId[0]);
-        let gameUrl = `board?b=${userBoard}&u=${userId[0].id}`;
-        console.log(baseUrl+gameUrl);
-        location.replace(baseUrl+gameUrl);
-    }else{
-        let gameUrl= "?m=ERROR"
-        location.replace(baseUrl+gameUrl);
+        gameUrl = `board?b=${userBoard}&u=${userId[0].id}`;
+        console.log(`1. ${baseUrl+gameUrl}`);
     }
+    //location.replace(baseUrl+gameUrl);
+    return gameUrl;
 }

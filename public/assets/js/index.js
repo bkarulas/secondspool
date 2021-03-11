28c4c60fed7facd9d3c0cef6e48d7126cd7fd921
Brad <bkarulas@outlook.com>
2021-03-11 14:47:19 -0500

    beta version 1 updates

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,14 +1,17 @@
 // const theMainUrl = window.location.href;
 // const theUrl = theMainUrl.substring(0,theMainUrl.indexOf('/?'));
-const theUrl = "http://hkypool.com"
+const theUrl = "http://localhost:8080"
+// const theUrl = "http://hkypool.com"
 
 let inputemail = document.getElementById('inputemail');
 let inputboard = document.getElementById('inputboard');
 
 
 async function enterPool(){
-    let userEmail = inputemail.value;
-    let userBoard = inputboard.value;
+    let userEmail = '';
+    let userBoard = '';
+    userEmail = inputemail.value;
+    userBoard = inputboard.value;
     let userId = await getUserId(userEmail, userBoard);
     //let gameUrl='';
     (userId.length>0)?location.replace(theUrl+`/board?b=${userBoard}&u=${userId[0].id}`):location.replace(theUrl);
